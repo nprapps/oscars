1bc1731ba74e849f0d277327d9598518a8826bea
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-02-21 12:22:39 -0500

    Who has two thumbs and is bad at coding? This guy.

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from glob import glob
+import json
 import os
 
 from fabric.api import *
@@ -307,10 +308,42 @@ def update_csv():
         f.write(response.text)
 
 
+def build_awards_json():
+    url = 'https://spreadsheets.google.com/feeds/list/0AjWpFWKpoFHqdDZHaTExd1Rpcl9aLTFIaVhIR2RRdWc/od6/public/values?alt=json-in-script&sq='
+    r = requests.get(url)
+
+    awards_list = []
+
+    if r.status_code == 200:
+        json_data = json.loads(r.content.replace('gdata.io.handleScriptLoaded(', '').replace(');', ''))
+        for row in json_data['feed']['entry']:
+            award_dict = {}
+            award_dict['award'] = row['title']['$t']
+            award_dict['nominees'] = []
+            award_dict['has_winner'] = False
+            for nominee_number in range(1, 10):
+                nominee = row['gsx$nominee%s' % nominee_number]['$t']
+                if nominee != u'':
+                    nominee_dict = {}
+                    nominee_dict['title'] = nominee
+                    nominee_dict['winner'] = False
+                    if row['gsx$winner']['$t'] == nominee:
+                        nominee_dict['winner'] = True
+                        award_dict['has_winner'] = True
+                    award_dict['nominees'].append(nominee_dict)
+
+            awards_list.append(award_dict)
+
+    with open('www/live-data/awards.json', 'w') as f:
+        f.write(json.dumps(awards_list))
+
+
 def deploy_awards_json():
     """
     Deploy awards JSON to S3
     """
+    build_awards_json()
+
     s3cmd = 's3cmd -P --add-header=Cache-Control:max-age=5 --guess-mime-type put /home/ubuntu/apps/oscars/repository/www/live-data/awards.json %s'
 
     if os.environ['DEPLOYMENT_TARGET'] == 'production':
