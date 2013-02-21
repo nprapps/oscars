75cf403deae42710f621928d35c23ac2e5e1c7b4
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-02-21 09:37:34 -0500

    Update winners data URL.

--- www/js/winners.js
+++ www/js/winners.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var $awards_list = $('.awards-list');
 	var template_html = '<h3>Awards List</h3>';
 
-    $.ajax('../live-data/awards.json', {
+    $.ajax('live-data/awards.json', {
         dataType: 'json',
         cache: false,
         crossDomain: false,
